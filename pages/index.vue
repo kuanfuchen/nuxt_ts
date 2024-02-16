82d11576a1f611c0ba5712fdb3a7cccd89bb3fdf
kuanfuchen <creazy_fu@hotmail.com>
2024-02-16 18:25:35 +0800

    add nuxtlink

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
     <h1>這是首頁</h1>
+    <div class="">
+      <NuxtLink to="/About">about</NuxtLink>
+    </div>
+    <div class="">
+      <NuxtLink to="/HomeView">homeview</NuxtLink>
+    </div>
+    
+    
   </div>
 </template>
